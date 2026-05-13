99871022516dc0b1c7e91aefbc8fc5bf88dfd06e
mohssine <mohssineechlaihi85@gmail.com>
2026-05-13 16:33:58 +0100

    task 5: Add shared backend foundation and fix GlobalExceptionHandler

--- backend/src/main/java/com/focusforge/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/focusforge/exception/GlobalExceptionHandler.java
@@ -2,21 +2,16 @@ package com.focusforge.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.Exception;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
-@RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestController
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception ex, WebRequest request) {
@@ -46,7 +41,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         public ErrorResponse(LocalDateTime timestamp, String message, String details) {
             this.timestamp = timestamp;
             this.message = message;
-            this.details = details;
+            this.details = message;
         }
 
         // Getters
